cae68a1f0bf1dcbcef5f544b19a9b0ebc144ec17
Aaron Parke <aaronjparke@gmail.com>
2019-02-06 19:40:43 -0600

    More changes for Digital Ocean

--- app.js
+++ app.js
@@ -2,9 +2,12 @@
 const express = require('express');
 const app = express();
 const router = require('./router');
+const PATH = require('path');
 
 // Setting up static directory - place all front-end resources in this folder
-app.use(express.static('public'));
+app.use('*/css',express.static('public/css'));
+app.use('*/front_js',express.static('public/front_js'));
+app.use('*/img',express.static('public/img'));
 
 // Send all routes to the router.js file
 app.use('/', router);
